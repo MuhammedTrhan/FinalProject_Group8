03522e7380fb05960ce9741892f8d85fa5179f82
Muhammed Turhan <meheme4434@gmail.com>
2026-09-02 12:54:48 +0300

    ADDED slash anim to player
    Added animations for opening and closing doors. Also updated player to stop during the interaction.

--- Scripts/PlayerAnimationHandler.gd
+++ Scripts/PlayerAnimationHandler.gd
@@ -1,18 +1,30 @@
 extends AnimationPlayer
 class_name PlayerAnimationHandler
 
+signal interact_anim_finish()
+
 @export var anim_player: AnimationPlayer
+
+var is_interacting: bool = false
+var interact_state: String = ""
+
 var last_direction: String = "down"
+
+# These interaction anims should override the default walk/idle animations
+var interaction_anims = ["slash", "back_slash", "lock", "unlock"]
 
 # Call this function from your Player's _physics_process
 func update_animations(velocity: Vector2) -> void:
     var state = "idle"
     
-    # 1. Determine if we are moving or idling
-    if velocity.length() > 0:
-        state = "walk"
-        # 2. Update the facing direction based on the strongest movement axis
-        last_direction = get_facing_direction(velocity)
+    if is_interacting:
+        state = interact_state
+    else:
+        # If not interacting, determine if we are walking or idling
+        if velocity.length() > 0:
+            state = "walk"
+            # Update the facing direction based on the strongest movement axis
+            last_direction = get_facing_direction(velocity)
     
     # 3. Construct the animation name and play it
     var anim_name = state + "_" + last_direction
@@ -33,13 +45,23 @@ func set_facing_direction(direction: String) -> void:
         last_direction = direction
     
 func handle_door_interaction(interaction: int) -> void:
-    # TODO: Add those animations!
+    is_interacting = true
+
     match interaction:
         0: # DoorInteraction.OPEN
-            play("slash_" + last_direction)
+            interact_state = "slash"
         1: # DoorInteraction.CLOSE
-            play("back_slash_" + last_direction)
+            interact_state = "back_slash"
         2: # DoorInteraction.LOCK
-            play("lock_" + last_direction)
+            interact_state = "lock"
         3: # DoorInteraction.UNLOCK
-            play("unlock_" + last_direction)
+            interact_state = "unlock"
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+    for anim in interaction_anims:
+        if anim_name.begins_with(anim):
+            is_interacting = false
+            interact_state = ""
+            
+            emit_signal("interact_anim_finish")
